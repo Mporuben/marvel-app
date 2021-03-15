6438c0190e4b41e35d012861ffdc2dd6b748a39a
Matus Poruben <poruben.matus@gmail.com>
2021-03-15 23:30:05 +0100

    feature: favorite heroes

--- src/views/tabs/Favorite.vue
+++ src/views/tabs/Favorite.vue
@@ -3,18 +3,39 @@
     <ion-header>
       <ion-toolbar>
         <ion-title>My favorite heroes</ion-title>
+        <ion-badge slot="end">
+          {{favoriteHeroes.length}}
+        </ion-badge>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      
+      <HeroesList v-if="favoriteHeroes.length" :heroes="favoriteHeroes"/>
+      <div v-else>
+        <ion-item lines="none">
+          <ion-label color="primary" class="ion-text-center">You have 0 favorite heroes</ion-label>
+        </ion-item>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {IonBadge } from '@ionic/vue';
+
+import HeroesList from '@/plugins/hero/HeroesList.vue'
 
 export default  {
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonBadge, HeroesList },
+
+  computed: {
+    favoriteHeroes() {
+      return this.$store.state.favorite.heroes
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch('favorite/fetchFavoriteHeroes')
+  },
+
 }
 </script>
