6b294ee38f3a6dd685a49acf70dcb6fcbd53cfd8
Matus Poruben <poruben.matus@gmail.com>
2021-03-15 11:59:17 +0100

    refactor: clean up router default setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import Tabs from '../views/Tabs.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/tabs/search'
   },
   {
     path: '/tabs/',
@@ -13,20 +13,17 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/tabs/search'
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1.vue')
+        path: 'search',
+        component: () => import('@/views/Search.vue')
       },
       {
         path: 'tab2',
         component: () => import('@/views/Tab2.vue')
       },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3.vue')
-      }
+
     ]
   }
 ]
---
+++ src/views/Search.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+
+    <ion-header>
+      <ion-toolbar >
+        <ion-searchbar class="ion-margin-top"></ion-searchbar>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+
+      <ion-list>
+        
+        <ion-item lines="full">
+          <ion-label>test</ion-label>
+          <ion-buttons>
+            <ion-button>
+              <ion-icon :icon="heart" />
+            </ion-button>
+          </ion-buttons>
+
+        </ion-item>
+
+      </ion-list>
+      
+    </ion-content>
+
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar,  IonContent, IonSearchbar,  } from '@ionic/vue';
+import { heartDislike, heart } from 'ionicons/icons';
+
+export default  {
+  components: { IonHeader, IonToolbar,  IonContent, IonPage,  IonSearchbar},
+
+  setup() {
+    return { heartDislike, heart }
+  }
+}
+</script>
